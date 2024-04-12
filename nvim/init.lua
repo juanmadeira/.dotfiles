2cc7f71a0f316d270ebd9c9af7efe91517e8e5e3
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-04-12 13:32:31 -0300

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,3 @@
-# ~/AppData/Local/nvim/init.lua
-
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -13,6 +11,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
-require("vim-options")
 require("lazy").setup("plugins")
+
+-- vim-options
+require("vim-options")
