8b295f099d5ee22ece68e89d19129af91b548420
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-04-19 00:14:21 -0300

    nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,25 +15,23 @@ set.softtabstop = 4
 -- mappings
 vim.g.mapleader = " "
 
-map("n", "<C-w>", ":set wrap!<CR>", {}) -- toggle word wrapping
+map("n", "<A-z>", ":set wrap!<CR>", {}) -- toggle word wrapping
 map("n", "<C-b>", ":Neotree toggle<CR>", {}) -- toggle neotree
 
 -- plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
 -- temas
 cmd[[colorscheme tokyonight]]
-
-
