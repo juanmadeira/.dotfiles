5c59572e1f6816c7bb106f644d6251298429e536
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-06-02 19:53:27 -0300

    windows-setup

--- windows-setup.ps1
+++ windows-setup.ps1
@@ -1,23 +1,23 @@
 <#
-	Script de configuração do meu powershell.
+	script de configuração do meu windows (powershell)
 	
 	— juan.
-	Data: 2024-04-11
+	data: 2024-04-11
 
-	Última atualização: 2024-06-01
+	última atualização: 2024-06-02
 #>
 
-# Instalação do Scoop
+# instalação do scoop
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser; Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression;
 
-	# Importação dos pacotes do Scoop
+	# importação dos pacotes do scoop
 	scoop import "packages/scoop-packages.json";
 
-# Instalação do Chocolatey
+# instalação do chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'));
 
-	# Importação dos pacotes do Chocolatey
+	# importação dos pacotes do chocolatey
 	choco install "packages/chocolatey-packages.config";
 
-# Instalação do WinGet
+# instalação do winget
 Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe;
