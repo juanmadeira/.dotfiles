33ee97dd2d2a432e3316da088ae36f476985156a
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-06-03 01:13:43 -0300

    arch setup

--- arch-setup.sh
+++ arch-setup.sh
@@ -24,7 +24,22 @@ sudo pacman -S python python-pip python-pipx;
 pipx ensurepath;
 
 # wallpaper setting
-yay -S swww;
+
+    # compilar o swww do código fonte
+    cd /opt;
+    git clone https://github.com/LGFae/swww;
+    cd swww;
+    cargo build --release;
+    cd target/release;
+    sudo cp -r swww ~/.local/bin;
+    sudo cp -r swww-daemon ~/.local/bin;
+    cd ~;
+
+#
+# para fazer:
+# configurar o swww -> waypaper -> pywal
+#
+
 pipx install waypaper pywal colorz;
 wal --backend colorz -i ".dotfiles/wallpapers/DSCF9244-1.jpg";
 
