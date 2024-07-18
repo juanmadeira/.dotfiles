042a231df6a517cab23f3d87ec3f929dad75a1b8
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2024-07-18 11:25:22 -0300

    windows powershell

--- windows-powershell/Microsoft.PowerShell_profile.ps1
+++ windows-powershell/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,15 @@
 
 # starship
 Invoke-Expression (&starship init powershell);
+
+# chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# aliases
+function ~ { cd ~ }
+function .. { cd .. }
+function gt { D:\Documents\documentos\programas\_cli\gt\gt.exe }
+function caderninho { cd D:\Documents\documentos\caderninho }
