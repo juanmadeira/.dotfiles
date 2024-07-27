cb779d1a329b91339859ed08181ff81f32c4cb3a
juan <21133654+juanmadeira@users.noreply.github.com>
2024-07-27 01:37:48 -0300

    arch

--- _arch.sh
+++ _arch.sh
@@ -5,7 +5,7 @@
 #	— juan.
 #	data: 2024-06-02
 #
-#	última atualização: 2024-07-25
+#	última atualização: 2024-07-27
 #
 
 ############
@@ -28,6 +28,12 @@ sudo pacman -S nodejs npm;
 # php
 sudo pacman -S php apache mariadb;
 
+# grub
+sudo pacman -S grub-customizer;
+    # xhost si:localuser:root
+    # sudo -s
+    # grub-customizer
+
 # fontes e icones
 yay -S hack-nf-mono-git hack-nf-git ttf-ubuntu-mono-nerd ttf-spacemono ttf-fira-code r-fontawesome ttf-roboto-variable noto-fonts-cjk noto-fonts-emoji noto-fonts;
 sudo npm -g install material-icons@latest;
@@ -36,18 +42,14 @@ chmod +x fonts.sh;
 sudo ./fonts.sh;
 cd ~;
 
-# grub
-# xhost si:localuser:root
-# sudo -s
-# grub-customizer
-
 # cli apps
-sudo pacman -S btop eza man fzf starship neofetch onefetch github-cli lazygit;
-yay -S cava cbonsai cmatrix-neo-git pipes.sh; 
+yay -S btop eza man unzip tar fzf starship github-cli lazygit;
+yay -S neofetch onefetch cava cbonsai cmatrix-neo-git pipes.sh; 
 pipx install pycowsay;
 
 # outros pacotes
 sudo pacman -S firefox;
+yay -S hyprshot clipse;
 
 
 #################
@@ -67,15 +69,15 @@ sudo cp ~/.dotfiles/.config/sddm/Fonts/* /usr/share/fonts/;
 sudo cp ~/.dotfiles/.config/sddm.conf /etc/;
 
 # thunar
-sudo pacman -S thunar;
+yay -S thunar;
 cp ~/.dotfiles/.config/xfce4 ~/.config/;
 
 # waybar
-sudo pacman -S waybar;
+yay -S waybar;
 cp -r ~/.dotfiles/.config/waybar ~/.config/;
 
 # rofi (menu launcher)
-yay -Sy rofi;
+yay -S rofi;
 cp -r ~/.dotfiles/.config/rofi ~/.config/;
 
 # swww e waypaper
