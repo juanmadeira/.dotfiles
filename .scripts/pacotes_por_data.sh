a67879a179ce9aab85e29ef88e13e64140db3698
juan <21133654+juanmadeira@users.noreply.github.com>
2024-08-10 00:26:18 -0300

    pacotes por data

--- .scripts/pacotes_por_data.sh
+++ .scripts/pacotes_por_data.sh
@@ -1,12 +1,16 @@
-echo ""
+#!/bin/bash
+# Esperar 1 segundo e limpa a tela
+sleep 1
+clear
 
+# Exibir "Pacotes por data" usando figlet
 figlet "Pacotes por data"
-
-echo ""
+echo
 
 for i in $(pacman -Qq)
 do
-  grep "\[ALPM\] installed $i" /var/log/pacman.log
+    grep "\[ALPM\] installed $i" /var/log/pacman.log
 done | \
-  sort -u | \
-  sed -e 's/\[ALPM\] installed //' -e 's/(.*$//'
+    sort -u | \
+    sed -e 's/\[ALPM\] installed //' -e 's/(.*$//'
+bash
