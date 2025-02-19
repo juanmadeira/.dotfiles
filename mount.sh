560d6ee6a0505063e7c3134ac45d5ae745c381d5
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-02-19 19:48:07 -0300

    substituídas binds de montagem por links simbólicos
    em bind não tem lixeira x(

--- mount.sh
+++ mount.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# script para facilitar a criação de diretórios e montagens ao fazer uma instalação limpa do sistema
+# script para facilitar a criacao de diretorios e links simbolicos ao fazer uma instalacao limpa do sistema
 
 sudo mkdir /eva01;
 sudo mkdir /eva02;
 sudo mkdir /eva03;
 sudo mkdir /eva04;
 
-sudo chmod o+rwx /eva01 -R -v
-sudo chmod o+rwx /eva02 -R -v
-sudo chmod o+rwx /eva03 -R -v
-sudo chmod o+rwx /eva04 -R -v
+sudo chmod o+rwx /eva01 -R -v;
+sudo chmod o+rwx /eva02 -R -v;
+sudo chmod o+rwx /eva03 -R -v;
+sudo chmod o+rwx /eva04 -R -v;
 
-sudo cat .etc/fstab-binds >> /etc/fstab;
+ln -s /eva02/documentos ~/; mv ~/documentos ~/Documents;
+ln -s /eva02/imagens ~/; mv ~/imagens ~/Pictures;
+ln -s /eva02/jogos ~/; mv ~/jogos ~/Games;
+ln -s /eva02/musicas ~/; mv ~/musicas ~/Music;
+ln -s /eva02/programas ~/; mv ~/programas ~/Programs;
+ln -s /eva04/biblioteca ~/; mv ~/biblioteca ~/Media;
 
 sudo mkdir -p /mnt/cdrom;
 sudo ln -s /run/user/1000/gvfs/cdda:host=sr0 /mnt/cdrom;
