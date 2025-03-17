2d9b3b3ff31ebbf7b36a471cde95f65afb1ab02c
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-03-17 02:32:06 -0300

    atualizados pacotes relacionados ao GTK

--- install.sh
+++ install.sh
@@ -45,7 +45,7 @@ sudo chmod +x ~/.config/rmpc/scripts/*;
 sudo chmod +x -R ~/*.AppImage
 
 # wallust, gtk, ly e grub
-yay --noconfirm -S wallust xdg-desktop-portal-gtk libportal-gtk3 libportal-gtk4 qt5ct qt6ct qt6-5compat qt6-declarative qt6-svg kvantum kvantum-qt5 ly;
+yay --noconfirm -S wallust gtk2 gtk3 gtk4 awf-gtk2 awf-gtk3 awf-gtk4 qt5ct qt6ct qt6-5compat qt6-declarative qt6-svg kvantum kvantum-qt5 ly;
 cp -r .local/share/themes ~/.local/share; # temas gtk
 cp -r .local/share/icons ~/.local/share; # icones gtk
 sudo ln -s ~/.local/share/themes/wallust/ /usr/share/themes; # link para o tema gtk
