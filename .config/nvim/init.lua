4f629e6e163c2731fceb07f265f3d1f6f7322bda
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-05-04 18:13:43 -0300

    adicionado plugin move

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -33,33 +33,53 @@ vim.cmd("hi BufferVisible guibg=transparent guifg=gray")
 
 -- keymaps
 local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
+    --- básicos
 map("n", "<C-s>", '<cmd>write<cr>', { desc = "Save" })
 map("n", "<A-z>", ":set wrap!<CR>", { desc = "Toggle wrap" })
-
+    
+    --- Neotree
 map("n", "<C-b>", ":Neotree toggle<CR>", { desc = "Toggle Neotree" })
 
+    --- Aerial
 map("n", "<C-a>", "<cmd>AerialToggle!<CR>", { desc = "Toggle Aerial" })
 on_attach = function(bufnr)
     map("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr, desc = "Previous Aerial" })
     map("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr, desc = "Next Aerial" })
 end
 
-map("n", "<Tab>", '<cmd>BufferNext<CR>', { desc = "Next Buffer" })
-map("n", "<S-Tab>", '<cmd>BufferPrevious<CR>', { desc = "Previous Buffer" })
-map("n", "<leader><Tab>", "<cmd>BufferClose<CR>", { desc = "Close Buffer" })
-map("n", "<leader><S-Tab>", "<cmd>BufferRestore<CR>", { desc = "Restore Buffer" })
+    --- barbar
+map("n", "<Tab>", '<cmd>BufferNext<CR>', opts, { desc = "Next Buffer" })
+map("n", "<S-Tab>", '<cmd>BufferPrevious<CR>', opts, { desc = "Previous Buffer" })
+map("n", "<leader><Tab>", "<cmd>BufferClose<CR>", opts, { desc = "Close Buffer" })
+map("n", "<leader><S-Tab>", "<cmd>BufferRestore<CR>", opts, { desc = "Restore Buffer" })
 
+    --- window-picker
 map("n", "<leader><Space>", "<cmd>lua require('nvim-window').pick()<CR>", { desc = "Jump to window" })
 
+    --- telescope
 local telescope = require('telescope.builtin')
 map("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" })
 map("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" })
 map("n", "<leader>fb", telescope.buffers, { desc = "Telescope buffer" })
 map("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
 
+    --- flash
 map({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
 map({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
 map("o", "r", function() require("flash").remote() end, { desc = "Remote Flash" })
 map({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
 map("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
+
+    --- move
+map('n', '<A-j>', ':MoveLine(1)<CR>', opts)
+map('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
+map('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
+map('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
+map('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
+map('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
+map('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
+map('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
+map('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
+map('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
