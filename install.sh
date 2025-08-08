441437d48b16dead7704d5e8b7f1487f0bdfff12
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-08-08 12:53:20 -0300

    removidos arquivos de configuração e pacotes inutilizados

--- install.sh
+++ install.sh
@@ -85,7 +85,7 @@ python -m venv ~/.venv/spotdl
 source ~/.venv/spotdl/bin/activate
     pip install spotdl # spotify downloader
 source ~/.venv/venv/bin/activate
-    pip install syncedlyrics eyed3 lyrics-in-terminal lyrics-in-terminal[mpd] # letras no terminal
+    pip install syncedlyrics eyed3  # letras no terminal
     pip install bandcamp-downloader # bandcamp downloader
  
 # instalar spicetify
