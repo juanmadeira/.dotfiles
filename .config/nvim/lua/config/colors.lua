228502411099540cb4656ed420428f6e320e1760
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-08-14 16:07:25 -0300

    removidas cores de plugin inutilizado

--- .config/nvim/lua/config/colors.lua
+++ .config/nvim/lua/config/colors.lua
@@ -4,8 +4,3 @@ vim.cmd("hi BufferTabpageFill guibg=transparent")
 vim.cmd("hi BufferCurrent guibg=transparent guifg=white")
 vim.cmd("hi BufferInactive guibg=transparent guifg=gray")
 vim.cmd("hi BufferVisible guibg=transparent guifg=gray")
-
-vim.cmd [[highlight Headline1 guibg=#1e2718]]
-vim.cmd [[highlight Headline2 guibg=#21262d]]
-vim.cmd [[highlight CodeBlock guibg=#1c1c1c]]
-vim.cmd [[highlight Dash guibg=#D19A66 gui=bold]]
