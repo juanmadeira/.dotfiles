9bfba9aa2fb27f215941a07136b3e32a6f6d8cdb
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2025-08-16 19:12:06 -0300

    atualizada config neovim da main

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -38,6 +38,7 @@ map("n", "<leader>nn", function()
     else vim.wo.relativenumber = true end
 end, { desc = "Toggle between relative line numbers" })
 
+-- delete file
 map("n", "<leader>df", function()
     local filename = vim.fn.expand("%:p")
     if vim.fn.confirm("Delete:\n" .. filename .. "?", "&Yes\n&No", 2) == 1 then
@@ -45,6 +46,47 @@ map("n", "<leader>df", function()
         vim.cmd("bd!")
     end
 end, { desc = "Delete current file" })
+
+-- open file
+map("n", "<leader>fo", function()
+    local col = vim.fn.col(".") - 1
+    local line = vim.fn.getline(".")
+    local file
+
+    local start = 0
+    while true do
+        local match = vim.fn.matchstrpos(line, "\\[[^]]*\\](\\([^)]*\\))", start)
+        if match[2] == -1 then break end
+        local s, e = match[2], match[3]
+        if col >= s and col < e then file = match[1]:match("%((.-)%)") break end
+        start = e
+    end
+
+    if not file then
+        local start2 = 0
+        while true do
+            local match = vim.fn.matchstrpos(line, "\\[\\[[^]]*\\]\\]", start2)
+            if match[2] == -1 then break end
+            local s, e = match[2], match[3]
+            if col >= s and col < e then file = match[1]:match("%[%[(.-)%]%]") break end
+            start2 = e
+        end
+    end
+
+    if not file or file == "" then file = vim.fn.expand("<cfile>") end
+    file = file:gsub("^%s+", ""):gsub("%s+$", "")
+    file = file:gsub("%%20", " ")
+
+    -- local base_dir = vim.fn.getcwd() -- caminho relativo ao cwd
+    local base_dir = vim.fn.expand("%:p:h") -- caminho relativo ao buffer
+    local full_path = vim.fn.resolve(base_dir .. "/" .. file)
+
+    if file:match("%.md$") then
+        vim.cmd("edit " .. vim.fn.fnameescape(full_path))
+    else
+        vim.fn.jobstart({ "xdg-open", full_path }, { detach = true })
+    end
+end, { desc = "Open file (Markdown, PDF, image etc.) under cursor" })
 
 --- Telescope
 map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Telescope find files" })
