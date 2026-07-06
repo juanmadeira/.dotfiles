6028597dd7da7c25eff8ea5567e06b4e4d71a22e
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2026-07-06 20:46:27 -0300

    feat(install): add yay build

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 DOTS="$HOME/.dotfiles"
 
+sudo pacman-key --populate
+sudo pacman --noconfirm -Sy --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay; makepkg -si; cd ..; rm -rf yay
+
 sudo mkdir -p /usr/share/wayland-sessions
 sudo cp $DOTS/dwl/dwl.desktop /usr/share/wayland-sessions
