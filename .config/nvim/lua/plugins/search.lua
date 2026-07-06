03fbf6528471b6b402cf597a57abce6086145edf
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2026-07-06 17:06:33 -0300

    chore(nvim): add new blink dependency

--- .config/nvim/lua/plugins/search.lua
+++ .config/nvim/lua/plugins/search.lua
@@ -4,6 +4,7 @@ vim.pack.add({
         { src = "https://github.com/MunifTanjim/nui.nvim" },
         { src = "https://github.com/nvim-tree/nvim-web-devicons" },
     { src = "https://github.com/saghen/blink.cmp" },
+        { src = "https://github.com/saghen/blink.lib" },
     { src = "https://github.com/nvim-telescope/telescope.nvim" },
     { src = "https://github.com/stevearc/aerial.nvim" },
         { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
