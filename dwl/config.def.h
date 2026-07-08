c3de14d1abe34003989356aa11140508a1d0be43
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2026-07-08 13:58:15 -0300

    chore(dwl): change default_opacity_onfocus to 0.9

--- dwl/config.def.h
+++ dwl/config.def.h
@@ -19,7 +19,7 @@ static const float focuscolor[]            = COLOR(0x005577ff);
 static const float urgentcolor[]           = COLOR(0xff0000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f}; /* You can also use glsl colors */
-static const float default_opacity_unfocus = 0.70f;
+static const float default_opacity_unfocus = 0.90f;
 static const float default_opacity_focus   = 1.00f;
 
 /* tagging - TAGCOUNT must be no greater than 31 */
