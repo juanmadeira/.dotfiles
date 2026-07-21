baf4fa1f105aba491754f16ced387f85a7b94a58
juanmadeira <21133654+juanmadeira@users.noreply.github.com>
2026-07-21 12:19:13 -0300

    chore(somebar): change font to JetBrainsMono

--- somebar/src/config.def.hpp
+++ somebar/src/config.def.hpp
@@ -10,7 +10,7 @@ constexpr int paddingX = 10;
 constexpr int paddingY = 3;
 
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
-constexpr const char* font = "Sans 12";
+constexpr const char* font = "JetBrainsMono Nerd Font 10";
 
 constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x22, 0x22, 0x22)};
 constexpr ColorScheme colorActive = {Color(0xee, 0xee, 0xee), Color(0x00, 0x55, 0x77)};
